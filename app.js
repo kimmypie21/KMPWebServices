a1ece817e9397c88b1fd97fe14287e91f0e89afb
Kimberly <kpierce865@yahoo.com>
2020-03-28 15:23:30 -0400

    commit

--- app.js
+++ app.js
@@ -14,17 +14,7 @@ app.get("/", function(request, response){
   response.render("index");
 });
 
-app.get("question1",function(request, response){
-  response.render("question1")
-});
 
-app.get("question2",function(request, response){
-  response.render("question2")
-});
-
-app.get("question3",function(request, response){
-  response.render("question3")
-});
 
 http.createServer(app).listen(8080, function(){
   console.log("Application is started on port 8080");
