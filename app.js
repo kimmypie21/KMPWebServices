b8f5c8bc62d36e84e84264e799c956715e93c319
Kimberly <kpierce865@yahoo.com>
2020-03-28 23:46:35 -0400

    commit

--- app.js
+++ app.js
@@ -2,17 +2,15 @@ var express = require("express");
 var http = require ("http");
 var path = require ("path");
 var logger = require ("morgan");
-
 var app = express();
 
-app.set("views", path.resolve(__dirname, "views"));
-app.set("view engine", "ejs");
 app.use(logger("short"));
 app.use(express.static (path.join(__dirname, "public")));
-
 app.get("/", function(request, response){
   response.render("index");
 });
+
+
 
 
 
