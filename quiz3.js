7b47c8e54fb2b4c495d9a9d5c80d1a7c9b79a724
Kimberly <kpierce865@yahoo.com>
2020-04-15 22:19:24 -0400

    commit

--- quiz3.js
+++ quiz3.js
@@ -1,38 +1,20 @@
 
 
 
+var QuizViewModel = ko.observableArray([
+  {AnswerA: "A: Periods",  AnswerB: "B: Colons" , AnswerC: "C: Semi-Colons"},
+  {AnswerA: "A: Examples",  AnswerB: "B: Containers", AnswerC:"C: Placeholders"},
+  {AnswerA: "A: A JavaScript object" , AnswerB: "B: A JavaScript expression" , AnswerC: "C: A JavaScript function"},
+  {AnswerA: "A: Storing and manipulating text" , AnswerB:  "B: Adding numbers",  AnswerC: "C: Concatenation"},
+  {AnswerA: "A: *" , AnswerB:  "B: + ",  AnswerC: "C: - "},
+  {AnswerA: "A: Document Objective Model",  AnswerB: "B: Document Oriented Model", AnswerC: "C: Document Object Model"},
+  {AnswerA: "A: True", AnswerB: "B: False", AnswerC: "C: Maybe"},
+  {AnswerA: "A: jQuery", AnswerB: "B: Node", AnswerC: "C: Ajax"},
+  {AnswerA: "A: It declares a function named database", AnswerB: "B: It declares a variable named database" , AnswerC: "C: It is a callback named database"},
+  {AnswerA: "A: 1" ,  AnswerB: "B: 11" , AnswerC: "C: 10"}
+]);
 
-var QuestionsViewModel = {
-  question1: "JavaScript statements are separated by what?",
-  question2: "JavaScript variables are __________ for storing data values." ,
-  question3: "________________ is a block of code designed to perform a particular task.",
-  question4: "What are JavaScript strings used for?",
-  question5: "What operator is used for concatenation?",
-  question6: "What does DOM stand for?",
-  question7: "JQuery is a JavaScript library.",
-  question8: "What is $ shorthand for in this statement: \n$('button').click.toggleClass('hidden')?",
-  question9: "What is the purpose of this JavaScript code: var = database?",
-  question10: "How many times does this loop run? \nfor(i=0, i<=10, i++) { \nconsole.log(i); \n};",
-  answer1: "A: Periods B: Colons C: Semi-Colons",
-  answer2:  "A: Examples B: Containers C: Placeholders",
-  answer3: "A: A JavaScript object B: A JavaScript expression C: A JavaScript function",
-  answer4: "A: Storing and manipulating text  B: Adding numbers  C: Concatenation",
-  answer5: "A: *  B: +  C: - ",
-  answer6: "A: Document Objective Model  B: Document Oriented Model C: Document Object Model",
-  answer7: "A: True B: False C: Maybe",
-  answer8: "A: jQuery B: Node C: Ajax",
-  answer9: 'A: It declares a function named database B: It declares a variable named database C: It is a callback named database',
-  answer10: "A: 1 B: 11 C: 10"
-  };
-
-  ko.applyBindings (QuestionsViewModel);
-
-
-
-
-
-
-
+  ko.applyBindings (new QuizViewModel ());
 
 
 
